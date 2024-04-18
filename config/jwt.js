5e07222e33640d09e535d5267774acf1a433862d
pradiptechie <kumarpradip3956@gmail.com>
2024-04-18 16:32:14 +0545

    updating refresh token

--- config/jwt.js
+++ config/jwt.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 module.exports = {
-  secretKey: process.env.secretKey,
+  secretKey: process.env.ACCESS_TOKEN_SECRET,
+  refreshKey: process.env.REFRESS_TOKEN_SECRET,
   expiresIn: process.env.expiresIn,
   OTPexpiresIn: process.env.OTPexpiresIn
 };

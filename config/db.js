6da5cf68a1d51f78823a663f958912538a3a6022
pradiptechie <kumarpradip3956@gmail.com>
2024-06-03 20:34:07 +0545

    remote mongodb server conf added

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
-
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const DB_URL = process.env.MONGO_DB_URL;
+// const DB_URL = process.env.MONGO_DB_REMOTE_URL;
+
 const connectDB = async () => {
   try {
-    await mongoose
-    .connect(process.env.MONGO_DB_URL)
-    .then(() => {
+    await mongoose.connect(DB_URL).then(() => {
       console.log("Let's Mongoose");
-    })
+    });
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     throw error;

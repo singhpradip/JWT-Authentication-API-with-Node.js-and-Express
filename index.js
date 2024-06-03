6aaddc2c59c63728f7ba3c00e48983be5ce2972f
pradiptechie <kumarpradip3956@gmail.com>
2024-06-03 20:47:59 +0545

    adding middlewares and handling uncatched errors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
-
 const express = require("express");
-const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
-const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,10 +12,20 @@ const PORT = process.env.PORT || 3000;
 // Connect to MongoDB
 connectDB();
 
-app.use(cors());
+// Middleware
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(express.json()); // Parse JSON bodies, result in 'req.body'
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies, result in 'req.body'
+app.use(cookieParser()); // Parse cookies, result in 'req.cookies'
 
-app.use(bodyParser.json());
-app.use("/", authRoutes);
+// Routes
+app.use("/auth", authRoutes);
+
+// Middle to handle uncatched error, yesle server shutdown hunw batw bachauxw
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ success: false, message: "Internal server error" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

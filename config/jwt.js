b0a74a379e7037578972d8d9d8afc6bb070ec023
pradiptechie <kumarpradip3956@gmail.com>
2024-06-03 20:36:52 +0545

    change in auth APIs

--- config/jwt.js
+++ config/jwt.js
@@ -1,8 +1,7 @@
-require('dotenv').config();
+require("dotenv").config();
 module.exports = {
   secretKey: process.env.ACCESS_TOKEN_SECRET,
   refreshKey: process.env.REFRESS_TOKEN_SECRET,
   expiresIn: process.env.EXPIRES_IN,
-  OTPexpiresIn: process.env.OTP_EXPIRES_IN
+  OTPexpiresIn: process.env.OTP_EXPIRES_IN,
 };
-  
